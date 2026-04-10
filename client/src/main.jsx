cddcdf12544f7a9cd0cbe3c963e970089b191145
Kamna chauhan <kamnac78@gmail.com>
2026-04-11 01:52:27 +0530

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,24 @@
+import { ClerkProvider } from "@clerk/clerk-react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add Your Clerk Publishable Key to the .env file");
+}
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" >
+   
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    
+  </ClerkProvider>
+  </StrictMode>
+);
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
+import Testimonial from "../components/Testimonial"
+import NewLetter from '../components/NewLetter'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <>
+        <Hero/>
+        <FeaturedDestination/>
+        <ExclusiveOffers/>
+        <Testimonial/>
+        <NewLetter/>
+        
+    </>
+  )
+}
+
+export default Home
